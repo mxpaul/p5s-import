ea2981cf72676c1bf11d9b0f52fb492a3bd0a990
User <mmonk@cpan.org>
2018-04-26 22:20:10 +0300

    Add more xml fields
    Add picture URLs, Prices, etc...

--- ex/fromfile/main.go
+++ ex/fromfile/main.go
@@ -16,14 +16,46 @@ type XMLka struct {
 	FileDate string    `xml:"FileDate,attr"`
 	Products []Product `xml:"product"`
 }
+
 type Product struct {
-	PictureSmall string `xml:"pictureSmall"`
-	Price        Price  `xml:"price"`
+	Name                 string     `xml:"name,attr"`
+	Description          string     `xml:"description"`
+	PictureSmall         string     `xml:"pictureSmall"`
+	Price                Price      `xml:"price"`
+	ProdId               string     `xml:"prodID,attr"`
+	Variants             []Variant  `xml:"assortiment>assort"`
+	Pictures             []Picture  `xml:"pictures>picture"`
+	VendorProdId         string     `xml:"vendorCode,attr"`
+	VendorName           string     `xml:"vendor,attr"`
+	VendorCollectionName string     `xml:"CollectionName,attr"`
+	Material             string     `xml:"material,attr"`
+	Diameter             float32    `xml:"diameter,attr"`
+	Length               float32    `xml:"lenght,attr"`
+	Categories           []Category `xml:"categories>category"`
 }
 
 type Price struct {
-	WholePrice     float32 `xml:"WholePrice,attr"`
-	BaseWholePrice float32 `xml:"BaseWholePrice,attr"`
+	WholePrice      float32 `xml:"WholePrice,attr"`
+	BaseWholePrice  float32 `xml:"BaseWholePrice,attr"`
+	BaseRetailPrice float32 `xml:"BaseRetailPrice,attr"`
+	RetailPrice     float32 `xml:"RetailPrice,attr"`
+	Discount        float32 `xml:"Discount,attr"`
+	StopPromo       float32 `xml:"StopPromo,attr"`
+}
+
+type Variant struct {
+	AID          string `xml:"aID,attr"`
+	Count        int    `xml:"sklad,attr"`
+	ShippingDate string `xml:"ShippingDate,attr"`
+}
+
+type Picture struct {
+	URL string `xml:",innerxml"`
+}
+
+type Category struct {
+	Name    string `xml:"Name,attr"`
+	SubName string `xml:"subName,attr"`
 }
 
 func main() {
