4e60d5f7b1d7b3a44e01eba5ae7c980f13f73b79
Maxim Polyakov <mmonk@cpan.org>
2018-05-27 22:35:28 +0300

    Save mocked mongo tests

--- import.go
+++ import.go
@@ -1,24 +1,29 @@
 package p5stock
 
 //go:generate sh -c "protoc *.proto --go_out=plugins=grpc:."
+//go:generate mockgen -source import.go -package mocks -destination mocks/storer.mock.go Storer
 
 import (
 	"context"
 	//"google.golang.org/grpc"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type Storer interface {
+	Upsert(interface{}, interface{}) (mgo.ChangeInfo, error)
+}
 
 type Stock struct {
 	// Mongo Connect
-	//Db *mgo.Collection
+	Db Storer
 }
 
 func (self *Stock) ImportProduct(ctx context.Context, prod *FullProduct) (*ImportReply, error) {
-	return &ImportReply{}, nil
-	//info, err := self.Db.Upsert(bson.M{"prodid": prod.ProdId}, &prod)
-	//if err != nil {
-	//	log.Printf("mongo error: %s", err)
-	//	return &stock.ImportReply{OK: false, Description: err.Error()}, err
-	//}
-	//log.Printf("Upsert [ok] %s %s M:%d U:%d", prod.ProdId, prod.Name, info.Matched, info.Updated)
-	//return &stock.ImportReply{OK: true}, nil
+	_, err := self.Db.Upsert(bson.M{"prodid": prod.ProdId}, prod)
+	if err != nil {
+		return &ImportReply{OK: false, Description: "database error"}, nil
+	}
+	return &ImportReply{OK: true}, nil
 }
--- import_test.go
+++ import_test.go
@@ -2,25 +2,69 @@ package p5stock
 
 import (
 	"context"
-	"google.golang.org/grpc"
+	"fmt"
 	"log"
 	"net"
 	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/kylelemons/godebug/pretty"
+	"google.golang.org/grpc"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/mxpaul/p5stock/mocks"
 )
 
 func serverAddr() string {
 	return "127.0.0.1:10203"
 }
 
-func TestDummy(t *testing.T) {
+func NewMockDb(t *testing.T, testCase *TestCase) (Done func(), mockDb *mocks.MockStorer, ctx context.Context) {
+	mockCtrl, ctx := gomock.WithContext(context.Background(), t)
+
+	mockDb = mocks.NewMockStorer(mockCtrl)
+	testCase.MockUpsertSetup(mockDb)
+
+	Done = func() {
+		mockCtrl.Finish()
+	}
+	return
+}
+
+func GoFuckers() {
+	_ = bson.M{}
 	_ = log.Printf
+}
+
+type TestCase struct {
+	Desc            string
+	Prod            *FullProduct
+	MockUpsertSetup func(mockDb *mocks.MockStorer)
+	Want            ImportReply
+}
+
+func RunTestCase(testCase *TestCase, t *testing.T) {
 	lis, err := net.Listen("tcp", serverAddr())
 	if err != nil {
 		t.Errorf("failed to listen: %v", err)
 	}
+
+	mockDone, mockStorer, ctx := NewMockDb(t, testCase)
+	defer mockDone()
+
 	grpcServer := grpc.NewServer()
-	RegisterStockServer(grpcServer, &Stock{})
-	go grpcServer.Serve(lis)
+	RegisterStockServer(grpcServer, &Stock{Db: mockStorer})
+	go func(server *grpc.Server, lis net.Listener) {
+		grpcServer.Serve(lis)
+	}(grpcServer, lis)
+
+	go func(ctx context.Context, t *testing.T, server *grpc.Server) {
+		select {
+		case <-ctx.Done():
+			server.Stop()
+		}
+	}(ctx, t, grpcServer)
 
 	conn, err := grpc.Dial(serverAddr(), grpc.WithInsecure())
 	if err != nil {
@@ -28,12 +72,46 @@ func TestDummy(t *testing.T) {
 	}
 	client := NewStockClient(conn)
 
-	prod := FullProduct{ProdId: "123"}
-	reply, err := client.ImportProduct(context.Background(), &prod)
+	got, err := client.ImportProduct(context.Background(), testCase.Prod)
 	if err != nil {
 		t.Errorf("ImportProduct error: %s", err)
 	}
-	log.Printf("Reply: %#v", reply)
+	log.Printf("Reply: %#v", got)
+	if diff := pretty.Compare(got, testCase.Want); diff != "" {
+		t.Fatalf("grpc reply unexpected: %s", diff)
+	}
 
 	grpcServer.Stop()
+}
+
+func TestDummy(t *testing.T) {
+	prodId := "123"
+	cases := []TestCase{
+		TestCase{
+			Desc: "first test case",
+			Prod: &FullProduct{ProdId: prodId},
+			MockUpsertSetup: func(mockDb *mocks.MockStorer) {
+				mockDb.EXPECT().
+					Upsert(bson.M{"prodid": prodId}, &FullProduct{ProdId: prodId}).
+					Return(mgo.ChangeInfo{}, nil).
+					Times(1)
+			},
+			Want: ImportReply{OK: true},
+		},
+		TestCase{
+			Desc: "database upsert error",
+			Prod: &FullProduct{ProdId: prodId},
+			MockUpsertSetup: func(mockDb *mocks.MockStorer) {
+				mockDb.EXPECT().
+					Upsert(bson.M{"prodid": prodId}, &FullProduct{ProdId: prodId}).
+					Return(mgo.ChangeInfo{}, fmt.Errorf("fuckup")).
+					Times(1)
+			},
+			Want: ImportReply{OK: false, Description: "database error"},
+		},
+	}
+
+	for _, testCase := range cases {
+		RunTestCase(&testCase, t)
+	}
 }
